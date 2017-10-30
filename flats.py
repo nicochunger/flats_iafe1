9b7895f3ba4ae98734b4577c796b0f0a86bdcd6f
Nicolas Unger <nicounger94@gmail.com>
2017-10-30 10:05:08 -0300

    Manual de uso y pequenas modificaciones.

--- flats.py
+++ flats.py
@@ -3,6 +3,7 @@
 import csv
 import pprint
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 fecha = []
 horario = []
@@ -19,9 +20,6 @@ for row in data:
         horario.append(row[1])
         luz.append(row[8])
 
-fecha = fecha[7:]
-horario = horario[7:]
-luz = luz[7:]
 while 'Brightness Value' in luz:
     luz.remove('Brightness Value')
 
@@ -32,12 +30,10 @@ while 'Time' in horario:
     horario.remove('Time')
 luz = [int(i) for i in luz]
 
-#print fecha[0]+' '+horario[0]
-
 fecha_hora = []
 for i in range(len(horario)):
     fecha_hora.append(datetime.strptime(fecha[i]+' '+horario[i],'%Y-%m-%d %H:%M:%S'))
 
-#print fecha_hora[0]
-
-pprint.pprint(zip(fecha_hora, luz))
+plt.plot(fecha_hora, luz)
+plt.show()
+#pprint.pprint(zip(fecha_hora, luz))
