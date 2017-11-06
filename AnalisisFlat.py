6dae50c1930a60b53fbc1b6cb445d3bf25b734c0
Nicolas Unger <nicounger94@gmail.com>
2017-11-05 23:00:27 -0300

    Lei y mejore los programas de AnalisisFlat y MasterFlat. Faltaba documentacion.

--- AnalisisFlat.py
+++ AnalisisFlat.py
@@ -3,64 +3,77 @@
 Created on Thu Jul 23 18:32:04 2015
 
 @author: belen
+modificado por: nico
 """
 
 import numpy as np
 from astropy.io import fits
-
 import subprocess
 
 
 path_archivos=raw_input('Introducir directorio de los flats: ')
 archivos=raw_input('Introducir nombre de los flats: ')
 Luz='/home/telescopio/DATOS/DatosClima/CloudWatcher/'
-b=subprocess.check_output('ls '+path_archivos+archivos,shell=True)
-b=b.split('\n')
+imagenes = subprocess.check_output('ls '+path_archivos+archivos,shell=True).split('\n')
 
 
 def anotar(archivo,texto,f):
-    c = open(archivo, f)
-    c.write(texto+'\n')
-    c.close()
-    
+    ''' Escribe texto sobre un archivo.
+
+    (str) archivo: nombre del archivo donde se quiere escribir
+    (str) texto: texto que se va a escribir
+    (str) f: permiso del archivo (escritura: w, leer: r, etc) '''
+    c = open(archivo, f) # Abre el archivo
+    c.write(texto+'\n') # Escribe el texto sobre el archivo y crea una nueva linea
+    c.close() # Cierra el archivo
+
 
 ########################     Valor de Luz  ######################
 
 
 def luz(Hora,Dia):
+    ''' Extrae el valor de luz para un dia y horario especificado.
+
+    (int) Hora: horario
+    (int) Dia: fecha '''
     try:
-        
-        a=Dia[6:8]+Dia[4:6]+'-'+Dia[0:4]
-        f=open(Luz+a,'r')
-        lineas=f.readlines()
-        f.close()
-        
+        a = Dia[6:8] + Dia[4:6] + '-' + Dia[0:4]
+        # Convierte la fecha en un formato que se pueda leer
+        f = open(Luz+a,'r')
+        # Abre el archivo de la estacion meteorologica de la fecha correspondiende
+        lineas = f.readlines() # Lee el archivo
+        f.close() # Cierra el archivo
+
         for line in lineas[1:]:
-            d=float(line.split(',')[1].replace('"','').replace(':',''))
-            if d >= float(Hora):
+            hora = float(line.split(',')[1].replace('"','').replace(':',''))
+            # Extre el horario de cada linea
+            if hora >= float(Hora): # Compara con la hora que queremos ver
+                '''Si llega a un horario que sea posterior al que queremos ver
+                   extre el valor de luz y corta el loop. '''
                 return line.split(',')[8].replace('"','')
                 break
     except:
-        return '?'
+        return '?' # Si no puede leer el archivo con los datos de luz
 
 ##################################################################
 
 
 anotar(path_archivos+'resultados_'+archivos,'Archivo,Dia,Hora,Tiempo de exp,Valor medio,Desviacion estandar, Valor luz','w')
 
-for line in b:
-    if 'fits' in line:
-        line=line.replace(path_archivos,'')        
-        c=fits.open(path_archivos+line)
-        datos=c[0].data
-        c.close()
-        linea=line.split('_')
-        seg=linea[3].strip('s')
-        std=str(np.std(datos))
-        mean= str(np.mean(datos))
-        hora=linea[4][-6:]
-        dia=linea[4][:8]
-        datoluz=luz(hora,dia)
+for line in imagenes:
+    if 'fits' in line: # Checkea que el archivo sea una imagen .fits
+        line = line.replace(path_archivos,'') # Nombre del archivo
+        c = fits.open(path_archivos+line) # Abre la imagen
+        datos = c[0].data # Extrae los valores de cuentas de cada pixel
+        c.close() # Cierra la imagen
+        linea = line.split('_') # Separa el nombre del archivo en cada cosa
+        seg = linea[3].strip('s') # Tiempo de exposicion
+        std = str(np.std(datos)) # Desviacion estandar de las cuentas de todos los pixeles
+        mean = str(np.mean(datos)) # Valor medio de las cuentas
+        hora = linea[4][-6:] # Horario de la imagen
+        dia = linea[4][:8] # Dia de la imagen
+        datoluz = luz(hora,dia) # Valor de luz en ese momento
+        # Guarda los resultados
         anotar(path_archivos+'resultados_'+archivos,line+','+dia+','+hora+','+seg+','+mean+','+std+','+datoluz,'a')
 
 
--- MasterFlat.py
+++ MasterFlat.py
@@ -16,13 +16,17 @@ filtros=['1','2','3','5'] #Filtros
 #dir_entrada='/home/belen/Escritorio/Datos/' #En que carpeta estan los bias y sky para analizar
 
 #Eso da el directorio desde donde se llama al programa
-dir_entrada=subprocess.check_output('pwd',shell=True).strip('\n')+'/'
-dir_salida=dir_entrada
+dir_entrada = subprocess.check_output('pwd',shell=True).strip('\n')+'/'
+dir_salida = dir_entrada
 
 def master_bias(dir_entrada,dir_salida):
+    ''' Crea un archivo Master_Bias que es un promedio de todos los bias.
+
+    (str) dir_entrada: directorio donde estan las imagenes bias
+    (str) dir_salida: directorio donde se guarda el Master Bias '''
     try: #Trata de hacer eso, si sale un error (Es que no hay imagenes), sigue para adelante
         bias=subprocess.check_output('ls '+dir_entrada+s_bias+'*',shell=True).replace(dir_entrada,'').split('\n')
-        #Busca todos los archivos en la carpeta que se llaman bias*, los acomododa en una lista        
+        #Busca todos los archivos en la carpeta que se llaman bias*, los acomododa en una lista
         archivos=','.join(x for x in bias if '.fits' in x)
         #Junta todos los datos que sean .fits de la lista. Pone comas en el medio
         fecha=bias[0].split('_')[4][:-6]
@@ -34,16 +38,22 @@ def master_bias(dir_entrada,dir_salida):
         iraf.cd(dir_entrada)
         #cambia la carpeta en la que iraf analiza cosas
         iraf.imcombine(input=archivos,output=dir_salida+'Master_Bias_'+fecha,combine='median')
-        #combina las imagenes y le llama Master_Bias_fecha        
+        #combina las imagenes y le llama Master_Bias_fecha
         return True,dir_salida+'Master_Bias_'+fecha
         #Devuelve verdadero su pudo crear un master bias
     else:
         return False
         #Devuelve falso si no pudo crear un master bias
 
-def reduc_bias(flat_entrada,master_bias,realizar,salida): #Esto se usa para corregir flas por bias
+def reduc_bias(flat_entrada,master_bias,realizar,salida): #Esto se usa para corregir flats por bias
+    ''' Esta funcion se usa para corregir flats por bias.
+
+    (str) flat_entrada: archivo del flat a corregir
+    (str) master_bias: archivo master de los bias
+    (bool) realizar: boolean que dice si hay un master bias
+    (str) salida: directorio y nombre de la imagen corregida '''
     if realizar is True: #Si la variable realizar es verdadera (Esa es basicamente si hay un master bias)
-        iraf.imarith(flat_entrada,'-',master_bias,salida) #Le resta a los flats los bias
+        iraf.imarith(flat_entrada,'-',master_bias,salida) #Le resta los bias a los flats
         iraf.imdel(flat_entrada) #Elimina la imagen de entrada
 
 
@@ -51,9 +61,9 @@ def reduc_bias(flat_entrada,master_bias,realizar,salida): #Esto se usa para corr
 def master_flat(dir_entrada,dir_salida,filtro,check_bias,bias):
     try: #trata de hacer lo siguiente
         flat=subprocess.check_output('ls '+dir_entrada+s_flat+filtro+'*',shell=True).replace(dir_entrada,'').split('\n')
-        #Se fija los archivos que son flats de cada filtro, los acomoda.        
+        #Se fija los archivos que son flats de cada filtro, los acomoda.
         archivos=','.join( x for x in flat if '.fits' in x )
-        #Acomoda todos los flas que encuentras        
+        #Acomoda todos los flas que encuentras
         fecha=flat[1].split('_')[4][:-6]
         #Busca la fecha
         bolean=True
@@ -67,7 +77,7 @@ def master_flat(dir_entrada,dir_salida,filtro,check_bias,bias):
         iraf.imcombine(input=archivos,output=dir_salida+'temp',combine='median')
         #Combina imagenes de flats
         reduc_bias(dir_salida+'temp.fits',bias,check_bias,dir_salida+'temp2')
-        #Lo reduce por bias        
+        #Lo reduce por bias
         c=fits.open(dir_salida+'temp2.fits')
         datos=c[0].data
         c.close()
@@ -76,8 +86,9 @@ def master_flat(dir_entrada,dir_salida,filtro,check_bias,bias):
         iraf.cd(dir_salida)
         iraf.imarith('temp2.fits','/',mean,'Master_flat_'+filtro+'_'+fecha)
         iraf.imdel('temp2.fits')
-        #Elimina la imagen temporar
-        return 'Master Flat creado para el filtro '+filtro+' y guardado en '+dir_salida
+        #Elimina la imagen temporaria
+        #return 'Master Flat creado para el filtro '+filtro+' y guardado en '+dir_salida
+        return 'Master Flat creado para el filtro %s y guardado en %s' % (filtro, dir_salida)
     else:
         return 'No se encuentran flats suficientes para el filtro '+filtro+' en '+dir_entrada
 
@@ -85,4 +96,3 @@ def master_flat(dir_entrada,dir_salida,filtro,check_bias,bias):
 check_bias,bias=master_bias(dir_entrada,dir_salida)
 for i in filtros:
     master_flat(dir_entrada,dir_salida,i,check_bias,bias)
-    
